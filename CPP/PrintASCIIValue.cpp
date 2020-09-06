d8cc8adb2b4ed93451aed5ecb9b2f6365c8a74e6
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-09-06 14:37:49 +0900

    add some cpp files

---
+++ CPP/PrintASCIIValue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+    cout << "ASCII Value of " << c << " is " << int(c);
+    return 0;
+}
+
+// Enter a character: p
+// ASCII Value of p is 112
---
+++ CPP/PrintNumberEnteredbyUser.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+    // Prints sum 
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}
+
+// Enter two integers: 4
+// 5
+// 4 + 5 = 9
+
