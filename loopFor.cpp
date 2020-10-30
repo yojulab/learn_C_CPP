ba4791fa5f2d922bc32daf640ac211b090bb03e2
Jane Vallette <janevallette@Janes-MacBook-Air.local>
2020-10-30 21:30:11 +0900

    add function description

--- loopFor.cpp
+++ loopFor.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
    // for loop execution
-   for (int a = 10; a < 20; a = a + 1)
+   for (int first = 10; first < 20; first++)
    {
-      cout << "value of a: " << a << endl;
-      if (a > 16){   // with break statement
+      cout << "value of first: " << first << endl;
+      if (first > 16)
+      { // with break statement
          // terminate the loop
          break;
       }
    }
-
+   // cout << "finally value of first: " << first << endl;  // Error
    return 0;
 }
