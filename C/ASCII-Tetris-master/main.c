b64254239af468a46770a9e2c61290220b07f5e0
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-11-06 12:06:43 +0900

    change folders

--- C/ASCII-Tetris-master/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "tetris.h"
-
-int
-main(void) {
-    tetris_run(12, 15);
-    return EXIT_SUCCESS;
-}
--- C/linuxsystem/cwe_mallinfo.c
+++
@@ -1,8 +0,0 @@
-#include <malloc.h>
-#include <stdio.h>
-
-int main(void) {
-  struct mallinfo m = mallinfo();
-  printf("uordblks = %d\nfordblks = %d\n", m.uordblks, m.fordblks);
-  return 0;
-}
