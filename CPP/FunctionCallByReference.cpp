b70fdc779d8ec8ccef64e86dd44ce5fff61cfe7c
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-11-11 22:35:50 +0900

    change some files

--- CPP/FunctionCallByReference.cpp
+++ CPP/FunctionCallByReference.cpp
@@ -3,20 +3,26 @@ using namespace std;
  
 // function declaration
 void swap(int, int);
+void swapByReference(int &, int &);
  
 int main () {
    // local variable declaration:
    int first = 100;
    int second = 200;
  
-   cout << "Before swap, value of first :" << first << endl;
-   cout << "Before swap, value of second :" << second << endl;
- 
-   // calling a function to swap the values.
+   cout << "Before swap - first, second :" << first << ", " << second << endl;
+   // Before swap - first, second :100, 200
+   /* calling first function to swap the values.*/
    swap(first, second);
- 
-   cout << "After swap, value of first :" << first << endl;
-   cout << "After swap, value of second :" << second << endl;
+   cout << "After swap - first, second :" << first << ", " << second << endl;
+   // After swap - first, second :100, 200
+
+   cout << "Before swap by reference - first, second :" << first << ", " << second << endl;
+   // Before swap by reference - first, second :100, 200
+   /* calling function to swap by reference.*/
+   swapByReference(first, second);
+   cout << "After swap by reference - first, second :" << first << ", " << second << endl;
+   // After swap by reference - first, second :200, 100
  
    return 0;
 }
@@ -29,5 +35,16 @@ void swap(int x, int y) {
    x = y;    /* put y into x */
    y = temp; /* put x into y */
   
+   return;
+}
+
+void swapByReference(int &x, int &y)
+{
+
+   int temp;
+   temp = x; /* save the value at address x */
+   x = y;    /* put y into x */
+   y = temp; /* put x into y */
+
    return;
 }
