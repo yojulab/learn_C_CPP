5f6d6198e3c6d112861d8f962196937033fa6a98
sanghun oh <sanghunoh@sanghuns-MacBook-Air.local>
2020-11-15 23:11:25 +0900

    change some files

--- CPP/Vectors.cpp
+++ CPP/Vectors.cpp
@@ -5,6 +5,37 @@ using namespace std;
 
 int main(void)
 {
+
+    int row, column;
+    char char_row, char_column;
+
+    cout << "Enter Row and Column: ";
+    cin >> row >> column;
+    cout << "Display Row and Column : ";
+    cin >> char_row;
+
+    vector<char> vector_column;
+    vector<vector<char>> vector_matrix;
+
+    for (int i = 0; i < row; i++)
+        vector_matrix[i] = new char[column];
+
+    cout << "Matrix Size : " << row*column << endl;
+
+    // output each array element's value
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+        {
+            vector_matrix[i][j] = char_row;
+        }
+
+    // output each array element's value
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+        {
+            cout << "vector_matrix[" << i << "][" << j << "]: ";
+            cout << vector_matrix[i][j] << endl;
+        }
     auto il = {1, 2, 3, 4, 5};
     /* create vector from initializer list */
     vector<int> v(il);
